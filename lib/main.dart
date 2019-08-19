2e80b604ec5dbbd5935d3ad336cf6ae180e34a3f
Hazem Saleh <hazems@apache.org>
2019-08-18 20:27:43 -0400

    Knowledge Checker App

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,7 @@ class _HomeState extends State<Home> {
     }
 
     return new MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
               title: Text('Knowledge Checker!'),
