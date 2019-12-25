3a8ef6da51644a97f6887f22b953dc2d9908639a
Hazem Saleh <hazems@apache.org>
2019-12-24 23:16:44 -0500

    Upgrading Dart and Flutter

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hazemsaleh/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hazemsaleh/Desktop/NYC Flutter Meetup 2020/Demos/Flutter_Knowledge_Checker"
+export "FLUTTER_TARGET=/Users/hazemsaleh/Desktop/NYC Flutter Meetup 2020/Demos/Flutter_Knowledge_Checker/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hazemsaleh/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
